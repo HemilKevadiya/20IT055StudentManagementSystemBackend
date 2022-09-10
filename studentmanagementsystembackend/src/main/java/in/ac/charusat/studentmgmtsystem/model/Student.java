90735e7acfe72b5ffe5558a905a0c0cc2dc14c4e
Hemil321 <hkevadiya1104@gmail.com>
2022-09-10 13:35:48 +0530

    Added AWS RDS URL.

--- studentmanagementsystembackend/src/main/java/in/ac/charusat/studentmgmtsystem/model/Student.java
+++ studentmanagementsystembackend/src/main/java/in/ac/charusat/studentmgmtsystem/model/Student.java
@@ -1,2 +1,24 @@
-package in.ac.charusat.studentmgmtsystem.model;public class Student {
-}
+package in.ac.charusat.studentmgmtsystem.controller;
+package in.ac.charusat.studentmgmtsystem.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Student {
+
+    @Id
+    private Integer id;
+    private String name;
+    private String address;
+    public Student(Integer id, String name, String address) {
+        this.address = address;
+    }
+
+    public Student() {
+
+    }
+
+    public Integer getId() {
+        return id;
+    }
